bf1bd43c4940c88eceee753a0a63d0c5a912ab52
shakir.kamal <shakir.kamal@kendimed.com>
2024-04-07 14:05:44 +0200

    fixed the mobile nav to close on clicked outside

--- client/src/components/MobileNav.tsx
+++ client/src/components/MobileNav.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { NAV_LINKS } from "../constants";
+import React, { useEffect, useRef } from "react";
 
 interface MobileNavProps {
   setOpenMobileNav: (value: boolean) => void;
@@ -36,9 +37,28 @@ const MobileNav = ({ setOpenMobileNav, openMobileNav }: MobileNavProps) => {
     },
   };
 
+  function useOutsideAlerter(ref: React.RefObject<HTMLElement>) {
+    useEffect(() => {
+      function handleClickOutside(event: MouseEvent) {
+        if (ref.current && !ref.current.contains(event.target as Node)) {
+          setOpenMobileNav(false);
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
   return (
     <div className="flex justify-center fixed z-40 w-full  ">
       <motion.div
+        ref={wrapperRef}
         className="bg-white border border-gray-200 shadow-xl w-[80%] h-[200px]"
         initial="closed"
         animate={openMobileNav ? "open" : "closed"}
@@ -46,14 +66,15 @@ const MobileNav = ({ setOpenMobileNav, openMobileNav }: MobileNavProps) => {
       >
         <div className="flex flex-col gap-4 justify-center py-5 px-4 font-semibold">
           {NAV_LINKS?.map((nav) => (
-            <motion.span
+            <motion.a
+              href={nav.href}
               variants={spanVariants}
               className="cursor-pointer hover:text-gray"
               onClick={() => setOpenMobileNav(!openMobileNav)}
               key={nav.key}
             >
               {nav.label}
-            </motion.span>
+            </motion.a>
           ))}
         </div>
       </motion.div>
